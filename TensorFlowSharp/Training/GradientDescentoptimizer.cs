d1256095984f8f5df98c14f86c0cc2bf6d6c53f2
mada@ihfood.dk <mada@ihfood.dk>
2017-08-03 09:52:32 +0200

    Change learning rate in gradient descent optimizer to be of type TFTensor. Also added documentation

--- TensorFlowSharp/Training/GradientDescentoptimizer.cs
+++ TensorFlowSharp/Training/GradientDescentoptimizer.cs
@@ -3,16 +3,33 @@ using TensorFlow;
 
 namespace TensorFlowSharp.Training
 {
+    /// <summary>
+    /// Optimizer that implements the gradient descent algorithm.
+    /// </summary>
     public class GradientDescentOptimizer : Optimizer
     {
-        readonly double m_learningRate;
+        readonly TFTensor m_learningRate;
 
-        public GradientDescentOptimizer(double learningRate, bool useLocking = false, string name = "GradientDescent")
+        /// <summary>
+        /// Construct a new gradient descent optimizer.
+        /// </summary>
+        /// <param name="learningRate">A Tensor or a floating point value.The learning
+        /// rate to use.</param>
+        /// <param name="useLocking">If True use locks for update operations.</param>
+        /// <param name="name">Optional name prefix for the operations created when applying
+        /// gradients.Defaults to "GradientDescent".</param>
+        public GradientDescentOptimizer(TFTensor learningRate, bool useLocking = false, string name = "GradientDescent")
             : base(useLocking, name)
         {
             m_learningRate = learningRate;
         }
 
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="grad"></param>
+        /// <param name="var"></param>
+        /// <returns></returns>
         protected override TFOutput ApplyDense(TFOutput grad, TFOutput var)
         {
             throw new NotImplementedException();
@@ -23,6 +40,12 @@ namespace TensorFlowSharp.Training
             //    use_locking = self._use_locking).op;
         }
 
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="grad"></param>
+        /// <param name="handle"></param>
+        /// <returns></returns>
         protected override TFOutput ResourceApplyDense(TFOutput grad, IntPtr handle)
         {
             throw new NotImplementedException();
@@ -32,6 +55,13 @@ namespace TensorFlowSharp.Training
             //    grad, use_locking = self._use_locking)
         }
 
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="grad"></param>
+        /// <param name="handle"></param>
+        /// <param name="indices"></param>
+        /// <returns></returns>
         protected override TFOutput ResourceApplySparseDuplicateIndices(TFOutput grad, IntPtr handle, int[] indices)
         {
             throw new NotImplementedException();
@@ -39,6 +69,12 @@ namespace TensorFlowSharp.Training
             //    handle.handle, indices, -grad * self._learning_rate);
         }
 
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="grad"></param>
+        /// <param name="var"></param>
+        /// <returns></returns>
         protected override TFOutput ApplySparseDuplicateIndices(TFOutput grad, TFOutput var)
         {
             throw new NotImplementedException();
@@ -49,6 +85,10 @@ namespace TensorFlowSharp.Training
             //return var.scatter_sub(delta, use_locking = self._use_locking)
         }
 
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
         protected override TFOutput Prepare()
         {
             throw new NotImplementedException();
